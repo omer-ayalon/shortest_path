004a2c10fdfee8cbc32f09f42f537f966dcd6ec6
omer-ayalon <omer1ayalon@gmail.com>
2023-08-25 12:32:49 +0300

    More Organized

--- main.py
+++ main.py
@@ -1,58 +1,8 @@
-import numpy as np
 import sys
+from os import environ
+environ['PYGAME_HIDE_SUPPORT_PROMPT'] = '1'
 import pygame
-
-
-def find_neighbor(pos):
-    neighbors = []
-    if grid_map[pos[0]+1][pos[1]] == 0:  # Search Down
-        neighbors.append([pos[0]+1, pos[1]])
-    if grid_map[pos[0]-1][pos[1]] == 0:  # Search Up
-        neighbors.append([pos[0]-1, pos[1]])
-    if grid_map[pos[0]][pos[1]-1] == 0:  # Search Left
-        neighbors.append([pos[0], pos[1]-1])
-    if grid_map[pos[0]][pos[1]+1] == 0:  # Search Right
-        neighbors.append([pos[0], pos[1]+1])
-
-    return neighbors
-
-
-def find_path():
-    tmp = np.array(curr_pos)
-    path = [tmp]
-
-    while np.any(np.not_equal(path[-1], start_pos)):
-        path.append(priors[int(tmp[0])][int(tmp[1])])
-        tmp = priors[int(tmp[0])][int(tmp[1])]
-
-    path = path[0:-1]
-    return path
-
-
-def draw():
-    screen.fill('black')
-
-    for i in range(grid_map.shape[0]):
-        for j in range(grid_map.shape[1]):
-            if grid_map[i][j] == 1:
-                pygame.draw.rect(screen, 'purple', [j * a, i * a, a, a])
-
-    for pos in visited_pos:
-        pygame.draw.rect(screen, 'white', [pos[1] * a, pos[0] * a, a, a])
-
-    for pos in queue:
-        pygame.draw.rect(screen, 'grey', [pos[1] * a, pos[0] * a, a, a])
-
-    pygame.draw.rect(screen, 'green', [start_pos[1] * a, start_pos[0] * a, a, a])
-    pygame.draw.rect(screen, 'red', [end_pos[1] * a, end_pos[0] * a, a, a])
-
-    if not searching:
-        path = find_path()
-        for pos in path:
-            pygame.draw.rect(screen, 'blue', [pos[1] * a, pos[0] * a, a, a])
-
-    pygame.display.flip()
-
+from collections import deque
 
 pygame.init()
 
@@ -61,52 +11,143 @@ fpsClock = pygame.time.Clock()
 
 width, height = 640, 480
 screen = pygame.display.set_mode((width, height))
+pygame.display.set_caption('Path Finding - BFS')
 
-start_pos = [1, 1]
-end_pos = [5, 18]
+block_size = 20
+grid = [[0] * (width // block_size) for _ in range(height // block_size)]
+grid_for_path = [[0, 0] * (width // block_size) for _ in range(height // block_size)]
+
+start_block = [-1, -1]
+end_block = [-1, -1]
+
+state = 'draw'
+
 visited_pos = []
-queue = [start_pos]
-grid_map = np.array([[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-                     [1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-                     [1, 0, 1, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-                     [1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-                     [1, 0, 0, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1],
-                     [1, 0, 0, 0, 0, 0, 1, 0, 0, 1, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1],
-                     [1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1],
-                     [1, 0, 0, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1],
-                     [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1],
-                     [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1],
-                     [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-                     [1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-                     [1, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-                     [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-                     [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-                     [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]])
-priors = np.zeros([grid_map.shape[0], grid_map.shape[1], 2])
+queue = deque()
+
+path = []
 
 
-a = 25
-searching = True
-# Game loop.
-while True:
+def draw():
+    for i in range(height // block_size):
+        for j in range(width // block_size):
+            # Draw Barrier Blocks
+            if grid[i][j]:
+                pygame.draw.rect(screen, (100, 100, 100), [j * block_size, i * block_size, block_size, block_size])
+
+            # Draw The Queue In Green And Visited Positions In Red
+            if [i, j] in visited_pos:
+                pygame.draw.rect(screen, (255, 0, 0), [j * block_size, i * block_size, block_size, block_size])
+            if [i, j] in queue:
+                pygame.draw.rect(screen, (0, 255, 0), [j * block_size, i * block_size, block_size, block_size])
+
+            # Draw Start And End Positions
+            if start_block == [i, j]:
+                pygame.draw.rect(screen, (255, 255, 255), [j * block_size, i * block_size, block_size, block_size])
+            if end_block == [i, j]:
+                pygame.draw.rect(screen, (0, 0, 255), [j * block_size, i * block_size, block_size, block_size])
+
+            # Draw Grid Lines
+            if j == width // block_size - 1:
+                pygame.draw.line(screen, (255, 255, 255), [0, i * block_size], [width, i * block_size])
+            if i == height // block_size - 1:
+                pygame.draw.line(screen, (255, 255, 255), [j * block_size, 0], [j * block_size, height])
+
+    # Draw Path
+    if state == 'end':
+        for pos in path:
+            pygame.draw.rect(screen, (0, 255, 255), [pos[1] * block_size, pos[0] * block_size, block_size, block_size])
+
+
+def mouse_keyboard_handler():
+    global start_block, end_block, queue, state
     for event in pygame.event.get():
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_ESCAPE:
+                pygame.quit()
                 sys.exit()
+            if state == 'draw':
+                if event.key == pygame.K_RETURN:  # It's The Enter Button
+                    if start_block[0] >= 0 and end_block[0] >= 0:
+                        queue.append(start_block)
+                        state = 'algorithm'
+                if event.key == pygame.K_s:
+                    start_block = pygame.mouse.get_pos()
+                    start_block = [start_block[1] // block_size, start_block[0] // block_size]
 
-    # Update.
-    if searching:
-        curr_pos = queue.pop(0)
+                if event.key == pygame.K_e:
+                    end_block = pygame.mouse.get_pos()
+                    end_block = [end_block[1] // block_size, end_block[0] // block_size]
 
-        visited_pos.append(curr_pos)
-        neighbors = find_neighbor(curr_pos)
-        for neighbor in neighbors:
-            if neighbor not in queue and neighbor not in visited_pos:
-                if neighbor == end_pos:
-                    searching = False
-                    break
-                priors[neighbor[0]][neighbor[1]] = curr_pos
-                queue.append(neighbor)
+    if state == 'draw':
+        mouse_buttons = pygame.mouse.get_pressed()
+        if mouse_buttons[0]:
+            pos = pygame.mouse.get_pos()
+            grid[pos[1] // block_size][pos[0] // block_size] = 1
+        if mouse_buttons[2]:
+            pos = pygame.mouse.get_pos()
+            grid[pos[1] // block_size][pos[0] // block_size] = 0
 
-    draw()
-    fpsClock.tick(fps)
+
+def get_neighbors(pos):
+    neighbors = []
+    # Up
+    if pos[0] >= 0:
+        if not grid[pos[0] - 1][pos[1]]:
+            neighbors.append([pos[0] - 1, pos[1]])
+    # Down
+    if pos[0] < height // block_size - 1:
+        if not grid[pos[0] + 1][pos[1]]:
+            neighbors.append([pos[0] + 1, pos[1]])
+    # Right
+    if pos[1] < width // block_size - 1:
+        if not grid[pos[0]][pos[1] + 1]:
+            neighbors.append([pos[0], pos[1] + 1])
+    # Left
+    if pos[1] >= 0:
+        if not grid[pos[0]][pos[1] - 1]:
+            neighbors.append([pos[0], pos[1] - 1])
+
+    return neighbors
+
+
+def find_path():
+    arr = [end_block]
+    while arr[-1][0] != start_block[0] or arr[-1][1] != start_block[1]:
+        arr.append(grid_for_path[arr[-1][0]][arr[-1][1]])
+
+    arr = arr[1:-1]
+    return arr
+
+
+def algorithm_step():
+    global state, path
+    curr_pos = queue.popleft()
+    if curr_pos == end_block:
+        path = find_path()
+        state = 'end'
+    visited_pos.append(curr_pos)
+    neighbors = get_neighbors(curr_pos)
+    for neighbor in neighbors:
+        if neighbor not in visited_pos and neighbor not in queue:
+            queue.append(neighbor)
+            grid_for_path[neighbor[0]][neighbor[1]] = curr_pos
+
+
+def main():
+    while True:
+        screen.fill((0, 0, 0))
+
+        mouse_keyboard_handler()
+        if state == 'algorithm':
+            algorithm_step()
+
+        draw()
+
+        pygame.display.flip()
+        # if state != 'algorithm':
+        fpsClock.tick(fps)
+
+
+if __name__ == '__main__':
+    main()
